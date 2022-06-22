9fa2e8ac70478b31ab2b093b6b21a5be7175247b
Anish Rao <71076546+Anish2422@users.noreply.github.com>
2022-06-22 19:28:35 +0530

    Time: 0 ms (100.00%), Space: 7 MB (95.82%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void subset(int ind, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
+        
+        if (ind==arr.size()){
+            ans.push_back(ds);
+            return;
+        }
+        
+        ds.push_back(arr[ind]);
+        subset(ind+1, arr, ans, ds);
+        ds.pop_back();
+        
+        subset(ind+1, arr,ans,ds);
+        
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        subset(0,nums,ans,ds);
+        return ans;
+    }
+};
