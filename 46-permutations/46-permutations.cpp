60d94c846382fc739811d9e0a2a9e976df8c4c5d
Anish Rao <71076546+Anish2422@users.noreply.github.com>
2022-06-24 15:55:53 +0530

    Time: 3 ms (80.83%), Space: 7.7 MB (78.32%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
-    void perm(vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds, int map[]){
-        if (ds.size()==arr.size()){
-            ans.push_back(ds);
+//     // APPROACH 1
+//     void perm(vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds, int map[]){
+//         if (ds.size()==arr.size()){
+//             ans.push_back(ds);
+//             return;
+//         }
+        
+//         for (int i = 0; i<arr.size(); i++){
+//             if (!map[i]){
+//                 ds.push_back(arr[i]);
+//                 map[i]=1;
+//                 perm(arr,ans,ds,map);
+//                 ds.pop_back();
+//                 map[i]=0;
+//             }
+            
+//         }
+    
+        // APPROACH 2
+    
+        void perm(int ind, vector<int> &arr, vector<vector<int>> &ans){
+            
+        if (ind == arr.size()){
+            ans.push_back(arr);
             return;
         }
         
-        for (int i = 0; i<arr.size(); i++){
-            if (!map[i]){
-                ds.push_back(arr[i]);
-                map[i]=1;
-                perm(arr,ans,ds,map);
-                ds.pop_back();
-                map[i]=0;
-            }
-            
+        for (int i = ind; i<arr.size(); i++){
+            swap(arr[ind],arr[i]);
+            perm(ind+1, arr, ans);
+            swap(arr[ind],arr[i]);
         }
+    
     }
+    
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int n = nums.size();
-        int map[n];
-        for (int i = 0; i<n; i++) map[i] = 0;
-        perm(nums,ans,ds,map);
+        // //APPROACH 1
+        // vector<int> ds;
+        // int n = nums.size();
+        // int map[n];
+        // for (int i = 0; i<n; i++) map[i] = 0;
+        // perm(nums,ans,ds,map);
+        
+        perm(0,nums,ans);
         return ans;
     }
 };
