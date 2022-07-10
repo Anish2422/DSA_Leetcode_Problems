2c9c39f2067fd90685446ef2d1e7faa255ce252d
Anish Rao <71076546+Anish2422@users.noreply.github.com>
2022-07-10 11:35:34 +0530

    Time: 3 ms (38.80%), Space: 6.5 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> tri = {{1}};
-        for (int i = 1; i < numRows; i++){
-            vector<int> row (i+1, 0);
-            for (int j = 0; j<row.size(); j++){
-                if (j==0 || j==row.size()-1) row[j] = 1;
-                else if (j>0 && j<tri[i-1].size()){
-                    row[j] = tri[i-1][j-1] + tri[i-1][j];
-                }
+        // vector<vector<int>> tri = {{1}};
+        // for (int i = 1; i < numRows; i++){
+        //     vector<int> row (i+1, 0);
+        //     for (int j = 0; j<row.size(); j++){
+        //         if (j==0 || j==row.size()-1) row[j] = 1;
+        //         else if (j>0 && j<tri[i-1].size()){
+        //             row[j] = tri[i-1][j-1] + tri[i-1][j];
+        //         }
+        //     }
+        //     tri.push_back(row);
+        // }
+        // return tri;
+        
+        vector<vector<int>> tri(numRows);
+        for (int i = 0; i<numRows; i++){
+            tri[i].resize(i+1);
+            tri[i][0] = tri[i][i] = 1;
+            
+            for (int j = 1; j < i; j++){
+                tri[i][j] = tri[i-1][j-1] + tri[i-1][j];
             }
-            tri.push_back(row);
         }
+        
         return tri;
     }   
 };
